382374e2fd632cf9e84aebb60e7ee2234308e24f
chiko <chiko@DESKTOP-2RAT2QG>
2023-03-16 19:34:09 +0400

    Task_1 Finished

---
+++ Task_1/Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter text : ");
+string text = Console.ReadLine();
+bool isPalindrome = IsPalindrome(text);
+Console.WriteLine($"Is Palindrome? {isPalindrome}");
+
+static bool IsPalindrome(string text)
+{
+    if (text.Length <= 1)
+        return true;
+    else
+    {
+        if (text[0] != text[text.Length - 1])
+            return false;
+        return IsPalindrome(text.Substring(1, text.Length - 2));
+    }
+
+}
