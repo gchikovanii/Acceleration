2250dcb9dd3864de2765b6ff89b456e9fd9f2a9b
chiko <chiko@DESKTOP-2RAT2QG>
2023-03-17 13:27:21 +0400

    Task_5 Finished

---
+++ Task_5/Task_5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine(CountVariants(5));
+Console.WriteLine(CountVariants(2));
+Console.WriteLine(CountVariants(1));
+static int CountVariants(int stairCount)
+{
+    int c = 1;
+    int temp = 1;
+    int current;
+    for (int i = 2; i <= stairCount; i++)
+    {
+        current = c + temp;
+        temp = c;
+        c = current;
+    }
+    return c;
+}
+
