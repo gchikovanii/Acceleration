0e4ef61c9739aa440e517c641a90f196adcb2e7e
chiko <chiko@DESKTOP-2RAT2QG>
2023-03-17 13:00:17 +0400

    Task_3 Finished

---
+++ Task_3/Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = { 12, 1, 2, -19, 3,4,5, 6, 7, 8, 9 };
+Console.WriteLine($"Min number which is not in massive is: {NotContains(numbers)}");
+static int NotContains(int[] array)
+{
+    int number = 1;
+    while (true)
+    {
+        if (!array.Any(i => i == number))
+            return number;
+        number++;
+    }
+}
