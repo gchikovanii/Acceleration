89a267cc44bd683e259d3cc24d2bd42b2de22e57
chiko <chiko@DESKTOP-2RAT2QG>
2023-03-17 13:13:28 +0400

    Task_4 Finished

---
+++ Task_4/Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(IsProperly("(()())"));
+Console.WriteLine(IsProperly("()())"));
+Console.WriteLine(IsProperly("(5*5(3+4))"));
+
+static bool IsProperly(string sequence)
+{
+    int count = 0;
+    for (int i = 0; i < sequence.Length; i++)
+    {
+        if (sequence[i] == '(')
+            count++;
+        if (sequence[i] == ')') 
+            count--;
+        if (count < 0)
+            return false;
+    }
+    return true;
+   
+}
