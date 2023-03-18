0ccf6b5f1c72cfd396ce5e2f42feeb3dc5153a34
chiko <chiko@DESKTOP-2RAT2QG>
2023-03-18 23:42:24 +0400

    Task_8 Finished

---
+++ Task_8/Task_8/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8
+{
+    public class Country
+    {
+        public Name name { get; set; }
+        public string region { get; set; }
+        public string subregion { get; set; }
+        public float[] latlng { get; set; }
+        public double area { get; set; }
+        public int population { get; set; }
+    }
+}
---
+++ Task_8/Task_8/Program.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics.Metrics;
+using System.IO;
+using Task_8;
+
+#region Version1 void Type
+//GenerateCountryDataFiles();
+//void GenerateCountryDataFiles()
+//{
+//    foreach (var item in country)
+//    {
+//        var countryName = item.name.common;
+//        using (StreamWriter sr = new StreamWriter(path + countryName + ".txt"))
+//        {
+//            var region = item.region;
+//            var subregion = item.subregion;
+//            var area = item.area;
+//            var population = item.population;
+//            var latlng = item.latlng;
+//            float firstNum = latlng[0];
+//            float secondNum = latlng[1];
+//            sr.WriteLine($"Country: {countryName} Region: {region}" +
+//            $" subregion: {subregion} " +
+//            $"area: {area}" +
+//            $" population: {population} latlng: [{firstNum} , {secondNum}]");
+//        }
+//    }
+//}
+#endregion
+
+//Version 2
+class Program
+{
+    HttpClient client = new HttpClient();
+    string path = @"D:\Acceleration\Task_8\Task_8\Files\";
+    static async Task Main(string[] args)
+    {
+        Program program = new Program();
+        await program.GenerateCountryDataFiles();
+    }
+
+    public async Task GenerateCountryDataFiles()
+    {
+        string response = await client.GetStringAsync("https://restcountries.com/v3.1/all");
+        List<Country> country = JsonConvert.DeserializeObject<List<Country>>(response);
+        foreach (var item in country)
+        {
+            var countryName = item.name.common;
+            using(StreamWriter sr = new StreamWriter(path + countryName + ".txt"))
+            {
+                var region = item.region;
+                var subregion = item.subregion;
+                var area = item.area;
+                var population = item.population;
+                var latlng = item.latlng;
+                float firstNum = latlng[0];
+                float secondNum = latlng[1];
+                sr.WriteLine($"Country: {countryName} Region: {region}" +
+                $" subregion: {subregion} " +
+                $"area: {area}" +
+                $" population: {population} latlng: [{firstNum} , {secondNum}]");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
